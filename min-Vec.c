b14c8a0bedc737e6e7d9e875d06e45c8a73f35ee
Gabriel Ferraz Duque <83476335+gabrielferrazduque@users.noreply.github.com>
2025-09-18 16:30:25 -0300

    Update min-Vec.c

--- min-Vec.c
+++ min-Vec.c
@@ -45,7 +45,7 @@ int main() {
     }
 
     // Chamada da função
-    float menor = min_vet(v, n);
+    float menor = min_vet(v, n);   v = (float*) malloc(n*sizeof(float));    
     printf("\nO menor valor do vetor é: %.2f\n", menor);
 
     // Liberação da memória
